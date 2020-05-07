5a2c8841e45a9031b81a4e630add468ec9349683
Ben Graves <btg628@cs.bham.ac.uk>
2020-05-07 17:13:05 +0100

    style start page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import { Container, Row, Col } from 'reactstrap';
 
 import ConnectSpotify from "./components/ConnectSpotify/ConnectSpotify";
 import FindSongWithAccount from "./components/FindSong/FindSongWithAccount";
-import FindSongWithoutAccount from "./components/FindSong/FindSongWithoutAccount";
+import FindSongWithoutAccount from "./components/FindSong/FindSongWithoutAccount"
+
+import FullHeight from "react-full-height";
+
+import Logo from "./assets/logo.svg"
 
 class App extends Component {
 
@@ -17,6 +21,24 @@ class App extends Component {
     },
     connectedToSpotifyAccount: false
   }
+
+  onClickConnect = () => {
+    if (window.location.origin === "http://localhost:3000") {
+        console.log("here")
+        window.location = "http://localhost:8888/login"
+    } else {
+        window.location = "http://shower-song-backend.herokuapp.com/login"
+    }
+}
+
+onClickContinue = () => {
+    if (window.location.origin === "http://localhost:3000") {
+        console.log("here")
+        window.location = "http://localhost:8888/appauth"
+    } else {
+        window.location = "http://shower-song-backend.herokuapp.com/appauth"
+    }
+}
 
 
   componentDidMount() {
@@ -44,18 +66,39 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Container>
-          <Row>
-            <Col sm="12" md={{ size: 6, offset: 3 }}>
-              <h1>Shower Song</h1>
-              {this.state.accessToken ? 
-                this.state.connectedToSpotifyAccount ? 
-                <div><FindSongWithAccount accessToken={this.state.accessToken} /></div>
-                : <div><FindSongWithoutAccount accessToken={this.state.accessToken} /></div>
-                : <ConnectSpotify setConnected={connected => this.setConnected(connected)} />}
-            </Col>
-          </Row>
-        </Container>
+        {this.state.accessToken ?
+          this.state.connectedToSpotifyAccount ?
+            <div><FindSongWithAccount accessToken={this.state.accessToken} /></div>
+            : <div><FindSongWithoutAccount accessToken={this.state.accessToken} /></div>
+          :
+          <FullHeight className="start-page__background">
+            <Container>
+              <Row>
+                <Col sm="12" md={{ size: 6, offset: 3 }}>
+                  <img className="start-page__logo" src={Logo} alt="Shower Song Logo" />
+                  <div className="start-page__title">Shower Song</div>
+                  <div className="start-page__quote">
+                    “On average an eight minute shower uses around 65 litres of water, which is over three times the amount many people in the developing world rely on for a whole day”
+                </div>
+                  <div className="start-page__reference">
+                    -Tim Wainwright, Water Aid
+                </div>
+                  <div className="start-page__tagline">
+                    Find your perfect song for a shorter shower
+                </div>
+                  <div onClick={() => this.onClickConnect()} className="start-page__connect-button">
+                    Connect to Spotify
+                </div>
+                  <div className="start-page__or">
+                    or
+                </div>
+                  <div onClick={() => this.onClickContinue()}  className="start-page__continue-without-button">
+                    Continue without Spotify
+                </div>
+                </Col>
+              </Row>
+            </Container>
+          </FullHeight>}
       </div >
     );
   }
